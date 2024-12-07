ee953e5f1810ae80accb7f249fb4e50c0e09aa06
raidah05 <shahanasarwar.mmc38@gmail.com>
2024-12-07 16:25:23 +0600

    Python intro

--- python intro/main.py
+++ python intro/main.py
@@ -1,1 +1,14 @@
-print("Hello, My name is Raidah \n I read in Class 9 \n I love writing \n The sum of 3 + 5 is" , 3 + 5)
+print("My name is Raidah")
+# This is a sentence
+
+print(90)
+#This is a number
+
+print("Hello" \n "My name is rai")
+# we're using \n for line break
+
+print("The value is " , 21)
+# Writitng multiple arguments
+
+print("welcome to " end="!")
+
