30a8d1d74346869edfba30f7f6b4c032db99c92c
raidah05 <shahanasarwar.mmc38@gmail.com>
2024-12-07 16:35:13 +0600

    Variable

--- python intro/main.py
+++ python intro/main.py
@@ -4,11 +4,28 @@ print("My name is Raidah")
 print(90)
 #This is a number
 
-print("Hello" \n "My name is rai")
+print("Hello \n"  "My name is rai")
 # we're using \n for line break
 
 print("The value is " , 21)
 # Writitng multiple arguments
 
 print("welcome to " end="!")
+
+
+
+#Variables
+x = 18
+y = "Hayat"
+
+print(x)
+print(y)
+C = "codingal"
+
+print(C)
+
+# user input
+name = input("Enter Your Name: ")
+
+print("\n Hello," , name , "\n  Welcome to codingal")
 
