0d6c35455bffb47e5ec0c7148e4d0b5fd9873f3d
raidah05 <shahanasarwar.mmc38@gmail.com>
2024-12-08 15:11:10 +0600

    project

---
+++ python intro/Birthday_Store.py
@@ -0,0 +1,10 @@
+Rai_birthday = "18 dec"
+tuna_birthday = "16 june"
+sha_birthday = "05 oct"
+name = input("Enter you name:   ")
+day = input("Enter your Birthday date:  ")
+
+print( "Raidah's Birthday is" ,  Rai_birthday)
+print("Tuna's Birthday is" , tuna_birthday)
+print("Shaila's Birthday is" , sha_birthday)
+print(name,"'s Birthday is on" , day)
