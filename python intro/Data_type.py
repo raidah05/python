c42c667c77e4d67f243f61316456e3dbcfda73b9
raidah05 <shahanasarwar.mmc38@gmail.com>
2024-12-09 09:02:55 +0600

    Type-casting

--- python intro/Data_type.py
+++ python intro/Data_type.py
@@ -9,3 +9,29 @@ print("Data type of c : " , type(c))
 
 d = True 
 print("Data type of d :" , type(d))
+
+
+#Type casting 
+
+name = "Raidah"
+age = 16
+student = True
+weight = 45
+
+print("The name is " , name)
+print("The data type of name :" , type(name))
+
+print("Age is " , age)
+print("The data type of age :" , type(age))
+
+print("Are you student " , student)
+print("The data type of student :" , type(student))
+
+print("What is your weight " , weight)
+print("The data type of weight :" , type(weight))
+
+print("After Type-casting")
+age = float(age)
+print("The data type of age :" , type(age))
+weight = int(weight)
+print("The data type of weight :" , type(weight))
