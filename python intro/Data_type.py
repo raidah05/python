79b565a53d835e0580d3c1fe0548662fe759693a
raidah05 <shahanasarwar.mmc38@gmail.com>
2024-12-09 08:50:45 +0600

    Data type

---
+++ python intro/Data_type.py
@@ -0,0 +1,11 @@
+a = 5 
+print("Data type of a : " , type(a)) 
+
+b = 4.5
+print("Data type of b : " , type(b)) 
+
+c = "Raidah"
+print("Data type of c : " , type(c)) 
+
+d = True 
+print("Data type of d :" , type(d))
