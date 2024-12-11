6286a41f3cbd566efc2be2d06ba6ebdd9e2ff8d8
raidah05 <shahanasarwar.mmc38@gmail.com>
2024-12-11 09:22:55 +0600

    Season Teller

---
+++ python intro/Season_teller.py
@@ -0,0 +1,25 @@
+s = int(input("Enter the month in numerical form: "))
+if(s==1):
+    print("It is Winter season. Please wear warm cloths.")
+elif(s==2):
+    print("It is Winter season. Please wear warm cloths.")
+elif(s==11):
+    print("It is Winter season. Please wear warm cloths.")
+elif(s==12):
+    print("It is Winter season. Please wear warm cloths.")
+elif(s==3):
+    print("It is Spring season. You can wear any comfort cloths.")
+elif(s==4):
+    print("It is Spring season. You can wear any comfort cloths.")
+elif(s==5):
+    print("It is Summer season. Please wear light cloths.")
+elif(s==6):
+    print("It is Summer season. Please wear light cloths.")
+elif(s==7):
+    print("It is Rainy season. Please carry an umbrella.")
+elif(s==8):
+    print("It is Rainy season. Please carry an umbrella.")
+elif(s==9):
+    print("It is Autumn season. Make sure to enjoy the nature.")
+else:
+    print("It is Autumn season. Make sure to enjoy the nature.")
