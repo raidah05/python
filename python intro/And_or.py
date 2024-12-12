f75cff092b44fa35dab184b0e26456318272fd65
raidah05 <shahanasarwar.mmc38@gmail.com>
2024-12-12 10:32:45 +0600

    not equal

--- python intro/And_or.py
+++ python intro/And_or.py
@@ -6,3 +6,21 @@ if a > b or a > c:
     print("I am using  or operator")
 elif a < b and a > c:
     print("I am using and operator")
+
+#NOT Equal operator
+
+a = 10
+b = 12
+c = 12
+print(a != b)
+print(b != c)
+
+e = "Python"
+d = "Coding"
+if e!=d:
+    print(e,"and" , d, "are diffrent")
+
+a = 3
+b = 8
+if (a==2) != (b==8):
+    print("Hello")
