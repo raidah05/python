37f9973c477fcb613a8dfbae0816b6bec7f70540
raidah05 <shahanasarwar.mmc38@gmail.com>
2024-12-19 09:19:07 +0600

    sum of whole numbers

---
+++ loop/print.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a number: "))
+
+print("The printing range is from {0} to {1} :" .format(n,1))
+
+for i in range(n,0,-1):
+    print(i)
---
+++ loop/reversed_string.py
@@ -0,0 +1,7 @@
+n = input("Enter a string: ")
+string = ""
+
+for i in n:
+    string = i + string
+print("The orignal word " , n)
+print("The reversed word " , string)
