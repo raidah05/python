58885c221657712f1d58daf3f425cd432c057cd9
raidah05 <shahanasarwar.mmc38@gmail.com>
2024-12-24 19:34:24 +0600

    digit calculator

--- loop/digit_calculator.py
+++ loop/digit_calculator.py
@@ -1,12 +1,5 @@
-n = int(input("Enter a number: "))
+n = str(input("Enter a number: "))
 sum = 0
-temp = n
-
-while temp>=0:
-    digit = temp%10
-    if digit <=9:
-        sum = sum
-
-    sum= sum + 1
-    temp//=10
+for i in (n):
+    sum = sum + 1
 print("There are total ", sum ,"digits in the given number")
