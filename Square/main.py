d38423986a41cb159bad44ca908fc348e5ff1b84
raidah05 <shahanasarwar.mmc38@gmail.com>
2024-12-26 22:49:04 +0600

    square

---
+++ Square/main.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("Royal blue")
+turtle.Screen().title("Square")
+pen = turtle.Turtle()
+
+for i in range(4):
+    pen.forward(150)
+    pen.left(90)
+
+turtle.done()
