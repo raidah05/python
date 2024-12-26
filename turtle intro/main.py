a8864e45705395394438accf4c19ae64e239c340
raidah05 <shahanasarwar.mmc38@gmail.com>
2024-12-26 09:03:38 +0600

    turtle

---
+++ turtle intro/main.py
@@ -0,0 +1,15 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300,400)
+
+polygon = turtle.Turtle()
+
+num_side = 6
+len = 70
+angle = 360/ num_side
+
+for i in range(num_side):
+    polygon.forward(len)
+    polygon.right(angle)
+
+turtle.done()
