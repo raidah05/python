75aaa84297382f01428f2740fe3df007cd7a851f
raidah05 <shahanasarwar.mmc38@gmail.com>
2025-02-17 15:15:50 +0600

    tuple

---
+++ tuple/main.py
@@ -0,0 +1,15 @@
+x = ("tuple", False,3.2,5)
+print(x)
+
+y = x +(400,)
+print(y)
+
+z = (50,40,30,60,40,30,)
+print(z.count(30))
+
+a = (1,2,3,4,5,6,7,8,9,0)
+slice4 = a[4:7]
+print(slice4)
+
+slice40 = a[:9]
+print(slice40)
