9af351a23ca20bb14eaf59c60babc2e52a8ef16c
raidah05 <shahanasarwar.mmc38@gmail.com>
2025-02-17 15:26:13 +0600

    tuple

--- tuple/main.py
+++ tuple/main.py
@@ -13,3 +13,23 @@ print(slice4)
 
 slice40 = a[:9]
 print(slice40)
+
+
+#palindrome
+
+def palind(r):
+    e = len(r) - 1
+    s = 0 
+    while (s<e):
+        if(r[s] != r[e]):
+            return False
+        s +=1
+        e -=1
+    return True
+
+r =(1,2,3,4,5,5,4,3,2,1)
+
+if(palind(r)):
+    print("The tuple is flip-flop")
+else:
+    print("The tuple is not flip-flop")
