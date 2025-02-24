8676385b7a1b50bf84ae2254eef89ea183e701eb
raidah05 <shahanasarwar.mmc38@gmail.com>
2025-02-24 15:40:27 +0600

    country code

--- Get rid of duplicates/main.py
+++ Get rid of duplicates/main.py
@@ -23,3 +23,26 @@ for key,value in students_item.items():
         result[key] = value
 
 print(result)
+
+
+
+test = {'codingal': 2, 'is':2, 'best': 2, 'for': 2, 'coding': 1}
+print("The orignal dic:"+ str(test))
+k = 2 
+res = 0 
+for key in test:
+    if test[key]== k:
+        res = res +1
+
+print(res)
+
+
+country_code = {'India' : '0091',
+                'Bangladesh': '0880',
+                'Australia' : '0998'}
+
+print("Country code for India-")
+print(country_code.get('India','Not Found'))
+
+print("Country code for japan-")
+print(country_code.get('japan','Not Found'))
