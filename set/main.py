9cd12bc359d7a1db7ff7d67eea6a46e2834f8079
raidah05 <shahanasarwar.mmc38@gmail.com>
2025-03-05 09:52:46 +0600

    set

---
+++ set/main.py
@@ -0,0 +1,17 @@
+set0= {1,2,3}
+print(set0)
+
+set1= {1.0,'hello',(1,2,3)}
+print(set1)
+
+myset = {1,2,3,4,3212,4,3,2}
+print(myset)
+
+mysetm= set([1,2,3,4,5,32,1,4,1])
+print(mysetm,"\n")
+
+mysetn= set([1,2,4,5,32,1,4,1])
+print('The orignal set:', mysetn)
+mysetn.pop()
+print("After removing", mysetn)
+
