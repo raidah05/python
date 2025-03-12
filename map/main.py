308066a9e1693abf22d9c88350ea1711a33c6fec
raidah05 <shahanasarwar.mmc38@gmail.com>
2025-03-12 10:07:35 +0600

    zip it

--- map/main.py
+++ map/main.py
@@ -9,3 +9,22 @@ def sq(n):
     return n*n
 square = list(map(sq, num))
 print(square)
+
+
+#Zip it!!
+s1 ={2,3,4}
+s2= {'a','b','c'}
+s3 = list(zip(s1,s2))
+print(s3,'\n')
+
+l1 = [12,78,5]
+l2 = [100,200,300]
+
+for x ,y in zip(l1,l2[::-1]):
+    print(x,y)
+
+
+stocks = ['bata','zara','apex']
+prices = [2234,5689,8998]
+new_dict = {stocks : prices for stocks, prices in zip(stocks,prices)}
+print('\n{}'.format(new_dict))
