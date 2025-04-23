f0770138a334d3c2dd0d32b451bef0e221beea94
raidah05 <shahanasarwar.mmc38@gmail.com>
2025-04-23 15:17:26 +0600

    abstract

---
+++ abstract class/main.py
@@ -0,0 +1,15 @@
+from abc import ABC , abstractmethod
+class Absclass(ABC):
+    def print(self,x):
+        print("passed value:",x)
+    @abstractmethod
+    def task(self):
+        print("I am from absclass")
+
+class task_Class(Absclass):
+    def task(self):
+        print("I am from task_class")
+
+obj = task_Class()
+obj.print(100)
+obj.task()
