d47e8e98df0b029883988de1a75db1f9df4f76b3
raidah05 <shahanasarwar.mmc38@gmail.com>
2025-05-07 15:22:49 +0600

    pygame

---
+++ fruit quiz/main.py
@@ -0,0 +1,12 @@
+import random
+
+class fruitquiz:
+    def __init__(self):
+        
+        self.fruits = {'apple':'red',
+                       'orange':'orange',
+                       'strawberry':'pink',
+                       'banana':'yellow',
+                       'kiwi':'green',
+                       'pineapple':'yellow',}
+        
